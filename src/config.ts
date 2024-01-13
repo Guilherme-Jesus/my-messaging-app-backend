f13889de3da80f444196f622094528df974bb397
Guilherme-Jesus <guicoutoo@outlook.com>
2024-01-13 09:53:49 -0300

    [x] ajuste nas variaveis de ambiente

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,6 @@
+import serviceAccount from '../mensageria-b0e36-02471a185653.json'
 const admin = require('firebase-admin')
-// const serviceAccount = require("path/to/serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    type: process.env.FIREBASE_TYPE,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url:
-      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-  }),
+  credential: admin.credential.cert(serviceAccount),
 })
